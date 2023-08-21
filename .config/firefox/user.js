8faa12bcd72e7e217b297f72a5f0df88b5782c2e
ispanos <yiannisvspanos@gmail.com>
2023-08-22 00:56:39 +0300

    Update firefox configs

---
+++ .config/firefox/user.js
@@ -0,0 +1,56 @@
+
+user_pref("accessibility.typeaheadfind.flashBar", 0);
+user_pref("app.normandy.first_run", false);
+user_pref("app.normandy.migrationsApplied", 8);
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("browser.bookmarks.restore_default_bookmarks", false);
+user_pref("browser.bookmarks.showMobileBookmarks", true);
+user_pref("browser.contentblocking.category", "strict");
+user_pref("browser.newtabpage.pinned", "[
+{\"url\":\"https://www.youtube.com/\"},
+// {\"url\":\"https://mail.google.com/mail/u/0/#inbox\",\"label\":\"mail.google\"},
+{\"url\":\"https://www.reddit.com/\",\"label\":\"reddit\"},
+{\"url\":\"https://github.com/ispanos\",\"label\":\"github\"},
+{\"url\":\"https://twitter.com/\",\"label\":\"twitter\"},
+{\"url\":\"https://www.phoronix.com/scan.php?page=home\",\"label\":\"phoronix\"}
+]");
+user_pref("browser.protections_panel.infoMessage.seen", true);
+// user_pref("browser.startup.homepage", "file:///home/yiannis/Documents/Projects/startpage/startpage.html");
+user_pref("browser.tabs.drawInTitlebar", true);
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.warnOnOpen", false);
+user_pref("browser.uidensity", 1);
+user_pref("browser.urlbar.suggest.openpage", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionPolicyAcceptedVersion", 1);
+user_pref("doh-rollout.balrog-migration-done", true);
+user_pref("findbar.highlightAll", true);
+user_pref("font.internaluseonly.changed", true);
+user_pref("geo.enabled", false);
+user_pref("network.dns.disablePrefetch", true);
+user_pref("network.http.speculative-parallel-limit", 0);
+user_pref("network.predictor.cleaned-up", true);
+user_pref("network.predictor.enabled", false);
+user_pref("network.prefetch-next", false);
+user_pref("network.security.esni.enabled", true);
+user_pref("network.trr.mode", 2);
+user_pref("pref.downloads.disable_button.edit_actions", false);
+user_pref("pref.privacy.disable_button.cookie_exceptions", false);
+user_pref("privacy.cpd.offlineApps", true);
+user_pref("privacy.cpd.siteSettings", true);
+user_pref("privacy.donottrackheader.enabled", true);
+user_pref("privacy.sanitize.pending", "[{\"id\":\"newtab-container\",\"itemsToClear\":[],\"options\":{}}]");
+user_pref("privacy.sanitize.timeSpan", 0);
+user_pref("privacy.socialtracking.notification.counter", 2);
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("privacy.trackingprotection.socialtracking.enabled", true);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.hybridContent.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
