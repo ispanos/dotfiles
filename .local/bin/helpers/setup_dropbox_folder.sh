84208192cc4fad78466e22b8e9b8eed728eaf636
ispanos <yiannisvspanos@gmail.com>
2024-01-04 06:59:19 +0200

    fix dropbox path for links

--- .local/bin/helpers/setup_dropbox_folder.sh
+++ .local/bin/helpers/setup_dropbox_folder.sh
@@ -6,7 +6,9 @@ cd ~
 # DROPBOX_DIRS="$(locate -rb 'Dropbox$')"
 # echo $DROPBOX_DIRS
 
-DROPBOX_DIR="${DROPBOX_DIR:-$HOME/Dropbox}"
+# DROPBOX_DIR="${DROPBOX_DIR:-$HOME/Dropbox}"
+# DROPBOX_DIR='/var/mnt/970_evo/data/Dropbox'
+
 [ ! -d "$DROPBOX_DIR" ] && exit 1
 
 # xdg-user-dirs-update --set DOWNLOAD "$HOME/Downloads" # Default location
@@ -23,9 +25,8 @@ xdg-user-dirs-update --set MUSIC "$DROPBOX_DIR/Music"
 xdg-user-dirs-update --set PICTURES "$DROPBOX_DIR/Pictures"
 xdg-user-dirs-update --set VIDEOS "$DROPBOX_DIR/Videos"
 
-ln -s Dropbox/Videos .
-ln -s Dropbox/Documents .
-ln -s Dropbox/Pictures .
-ln -s Dropbox/Projects .
-ln -s Dropbox/Music .
-# ln -s .profile .zprofile
+ln -s "$DROPBOX_DIR/Videos" .
+ln -s "$DROPBOX_DIR/Documents" .
+ln -s "$DROPBOX_DIR/Pictures" .
+ln -s "$DROPBOX_DIR/Projects" .
+ln -s "$DROPBOX_DIR/Music" .
